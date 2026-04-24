7bb79db6d855f0fd4c20bc58d5883fb1717d971f
Kashino17 <tosun.media.services@gmail.com>
2026-04-24 19:20:15 +0200

    chore(android): add .gitignore, remove build artifacts from tracking

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_hikari_app_HikariApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.hikari.app.HikariApp"
-)
-public final class _com_hikari_app_HikariApp {
-}
